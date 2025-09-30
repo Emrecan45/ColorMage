5c8a525088906e130cc76ef3b6b218fe8cd630ea
Emrecan45 <demirtay.e@gmail.com>
2025-10-01 01:39:29 +0200

    feat: ajout de la configuration du jeu (couleurs, dimensions)

---
+++ src/config.py
@@ -0,0 +1,22 @@
+# ----- CONFIGURATION -----
+TAILLE_CELLULE = 40
+LARGEUR_GRILLE = 30
+HAUTEUR_GRILLE = 15
+
+LARGEUR_ECRAN = TAILLE_CELLULE * LARGEUR_GRILLE
+HAUTEUR_ECRAN = TAILLE_CELLULE * HAUTEUR_GRILLE
+
+COULEURS = {
+    "gris": (100, 100, 100),
+    "rouge": (255, 0, 0),
+    "bleu": (0, 0, 255),
+    "vert": (0, 255, 0),
+    "vide": (255, 255, 255),
+    "porte": (255, 255, 0),
+    "change_rouge": (255, 0, 0),
+    "change_bleu": (0, 0, 255),
+    "change_vert": (0, 255, 0),
+    "sol": (0, 0, 0),
+    "noir": (0, 0, 0),
+    "pic": (100, 100, 100)
+}
