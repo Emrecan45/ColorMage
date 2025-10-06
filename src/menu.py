4f714d6b2f68e22049d9960118af3f16efe61f86
Emrecan45 <demirtay.e@gmail.com>
2025-10-06 13:48:35 +0200

    Fix: espacement + son et musique dans parametres

--- src/menu.py
+++ src/menu.py
@@ -6,10 +6,11 @@ class Menu:
     """Menu du jeu"""
 
     def __init__(self):
-        self.font = pygame.font.SysFont(None, 50)
-        self.bouton_jouer = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 - 140, 250, 50)
-        self.bouton_parametres = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 - 60, 250, 50)
-        self.bouton_quitter = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 + 20, 250, 50)
+        self.font_1 = pygame.font.SysFont(None, 80)
+        self.font_2 = pygame.font.SysFont(None, 50)
+        self.bouton_jouer = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 - 80, 250, 50)
+        self.bouton_parametres = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 + 60, 250, 50)
+        self.bouton_quitter = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 + 200, 250, 50)
     
     def afficher_menu(self, ecran):
         """Affiche le menu"""
@@ -17,7 +18,7 @@ class Menu:
         ecran.fill((0, 0, 0))
         
         # titre du jeu
-        titre_txt = self.font.render("ColorMage", True, (255, 255, 255))
+        titre_txt = self.font_1.render("ColorMage", True, (255, 255, 255))
         ecran.blit(titre_txt, (LARGEUR_ECRAN // 2 - titre_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 220))
         
         # Dessiner les boutons
@@ -26,13 +27,13 @@ class Menu:
         pygame.draw.rect(ecran, (70, 70, 70), self.bouton_quitter)
         
         # Textes des boutons
-        jouer_txt = self.font.render("Jouer", True, (255, 255, 255))
-        parametres_txt = self.font.render("Paramètres", True, (255, 255, 255))
-        quitter_txt = self.font.render("Quitter", True, (255, 255, 255))
+        jouer_txt = self.font_2.render("Jouer", True, (255, 255, 255))
+        parametres_txt = self.font_2.render("Paramètres", True, (255, 255, 255))
+        quitter_txt = self.font_2.render("Quitter", True, (255, 255, 255))
         
-        ecran.blit(jouer_txt, (LARGEUR_ECRAN // 2 - jouer_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 130))
-        ecran.blit(parametres_txt, (LARGEUR_ECRAN // 2 - parametres_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 50))
-        ecran.blit(quitter_txt, (LARGEUR_ECRAN // 2 - quitter_txt.get_width() // 2, HAUTEUR_ECRAN // 2 + 30))
+        ecran.blit(jouer_txt, (LARGEUR_ECRAN // 2 - jouer_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 70))
+        ecran.blit(parametres_txt, (LARGEUR_ECRAN // 2 - parametres_txt.get_width() // 2, HAUTEUR_ECRAN // 2 + 70))
+        ecran.blit(quitter_txt, (LARGEUR_ECRAN // 2 - quitter_txt.get_width() // 2, HAUTEUR_ECRAN // 2 + 210))
         
         return None
     
--- src/parametres.py
+++ src/parametres.py
@@ -6,10 +6,17 @@ class Parametres:
     """Affiche les parametres et retourne l'action choisie"""
     
     def __init__(self):
-        self.font = pygame.font.SysFont(None, 50)
+        self.font_1 = pygame.font.SysFont(None, 80)
+        self.font_2 = pygame.font.SysFont(None, 50)
+        self.font_3 = pygame.font.SysFont(None, 40)
         self.overlay = pygame.Surface((LARGEUR_ECRAN, HAUTEUR_ECRAN))
         self.overlay.fill((0, 0, 0))
-        self.bouton_retour = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 + 20, 250, 50)
+        self.bouton_retour = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 + 200, 250, 50)
+        self.bouton_musique = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 - 90, 250, 15)
+        self.bouton_general = pygame.Rect(LARGEUR_ECRAN // 2 - 125, HAUTEUR_ECRAN // 2 + 0, 250, 15)
+        self.jauge_musique = 50 # Valeur au debut de la jauge musique
+        self.jauge_general = 50 # Valeur au debut de la jauge musique
+
 
     def afficher_parametres(self, ecran):
         """Affiche le menu et retourne l'action choisie
@@ -17,21 +24,37 @@ class Parametres:
         Returns:
             str: "jouer" ou "parametres" ou "quitter" ou None si toujours dans le menu
         """
-        #fond noir
+        # fond noir
         ecran.blit(self.overlay, (0, 0))
         
-        #titre du menu
-        titre_txt = self.font.render("Parametres", True, (255, 255, 255))
-        ecran.blit(titre_txt, (LARGEUR_ECRAN // 2 - titre_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 130))
+        # titre du menu
+        titre_txt = self.font_1.render("Paramètres", True, (255, 255, 255))
+        ecran.blit(titre_txt, (LARGEUR_ECRAN // 2 - titre_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 220))
         
-        #dessins des boutons
+        # version du jeu
+        version_txt = self.font_3.render("v1.0.0", True, (255, 255, 255))
+        ecran.blit(version_txt, (LARGEUR_ECRAN - version_txt.get_width() - 20, HAUTEUR_ECRAN - version_txt.get_height() - 20))
+
+        # volume musique
+        musique_txt = self.font_3.render("Volume Musique", True, (255, 255, 255))
+        ecran.blit(musique_txt, (LARGEUR_ECRAN // 2 - musique_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 130))
+        pygame.draw.rect(ecran, (100, 100, 100), self.bouton_musique)
+        # remplissage de la jauge
+        largeur_remplie = int((self.jauge_musique / 100) * self.bouton_musique.width)
+        pygame.draw.rect(ecran, (0, 200, 0),(self.bouton_musique.x, self.bouton_musique.y, largeur_remplie, self.bouton_musique.height))
+
+        # volume général
+        general_txt = self.font_3.render("Volume général", True, (255, 255, 255))
+        ecran.blit(general_txt, (LARGEUR_ECRAN // 2 - general_txt.get_width() // 2, HAUTEUR_ECRAN // 2 - 40))
+        pygame.draw.rect(ecran, (100, 100, 100), self.bouton_general)
+        # remplissage de la jauge
+        largeur_remplie = int((self.jauge_general / 100) * self.bouton_general.width)
+        pygame.draw.rect(ecran, (0, 200, 0),(self.bouton_general.x, self.bouton_general.y, largeur_remplie, self.bouton_general.height))
+        
+        # bouton retour
         pygame.draw.rect(ecran, (70, 70, 70), self.bouton_retour)
-        
-        #textes dans les boutons
-        retour_txt = self.font.render("Retour", True, (255, 255, 255))
-        
-        #affichage des textes dans les boutons
-        ecran.blit(retour_txt, (LARGEUR_ECRAN // 2 - retour_txt.get_width() // 2, HAUTEUR_ECRAN // 2 + 23))
+        retour_txt = self.font_2.render("Retour", True, (255, 255, 255))
+        ecran.blit(retour_txt, (LARGEUR_ECRAN // 2 - retour_txt.get_width() // 2, HAUTEUR_ECRAN // 2 + 210))
           
         return None
     
@@ -43,8 +66,14 @@ class Parametres:
             pos: Position du clic (x, y)
             
         Returns:
-            str: "quitter" ou None
+            str: "quitter" ou "volume_musique_modifié" ou "volume_général_modifié" ou None
         """
         if self.bouton_retour.collidepoint(pos):
             return "quitter"
+        if self.bouton_musique.collidepoint(pos):
+            self.jauge_musique = int(((pos[0] - self.bouton_musique.x) / self.bouton_musique.width) * 100)
+            return "volume_musique_modifié"
+        if self.bouton_general.collidepoint(pos):
+            self.jauge_general = int(((pos[0] - self.bouton_musique.x) / self.bouton_musique.width) * 100)
+            return "volume_général_modifié"
         return None
